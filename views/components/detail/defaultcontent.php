1e46f5139cc2dc30e21397ef9015e0cf34f0c3a9
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2022-10-25 11:42:46 +0700

    refactor vue-usage: default pages

---
+++ views/components/detail/defaultcontent.php
@@ -0,0 +1,21 @@
+<div class="alert alert-primary"><i class="bi bi-info-circle"></i> INFO: This is dummy preview</div>
+<div class="row mb-3">
+	<div class="col-md-3 col-lg-2 col-xl-1">Field 1</div>
+	<div class="col-md-6">Data 1</div>
+</div>
+<div class="row mb-3">
+	<div class="col-md-3 col-lg-2 col-xl-1">Field 2</div>
+	<div class="col-md">Data 2</div>
+</div>
+<div class="row mb-3">
+	<div class="col-md-3 col-lg-2 col-xl-1">Field 3</div>
+	<div class="col-md-5">Data 3</div>
+</div>
+<div class="row mb-3">
+	<div class="col-md-3 col-lg-2 col-xl-1">Field 3</div>
+	<div class="col-md-5">Data 4</div>
+</div>
+<div class="row mb-3">
+	<div class="col-md-3 col-lg-2 col-xl-1">Field 3</div>
+	<div class="col-md-5">Data 5</div>
+</div>
--- views/components/detail/defaultform.php
+++ views/components/detail/defaultform.php
@@ -1,4 +1,4 @@
-<div class="alert alert-danger">NOTE: This is dummy form</div>
+<div class="alert alert-primary"><i class="bi bi-info-circle"></i> INFO: This is dummy entry form</div>
 <div class="row mb-3">
 	<div class="col-md-3 col-lg-2 col-xl-1">Input 1</div>
 	<div class="col-md-6"><input class="form-control" /></div>
@@ -10,4 +10,20 @@
 <div class="row mb-3">
 	<div class="col-md-3 col-lg-2 col-xl-1">Input 3</div>
 	<div class="col-md-5"><input class="form-control" /></div>
+</div>
+<div class="row mb-3">
+	<div class="col-md-3 col-lg-2 col-xl-1">Input 3</div>
+	<div class="col-md-5">
+		<select name="" class="form-control">'
+			<option value="1">Option 1</option>
+			<option value="2">Option 2</option>
+			<option value="3">Option 3</option>
+		</select>
+	</div>
+</div>
+<div class="row mb-3">
+	<div class="col-md-3 col-lg-2 col-xl-1">Input 3</div>
+	<div class="col-md-5">
+		<textarea name="" class="form-control" rows="10"></textarea>
+	</div>
 </div>
