4d0b9fdef7397537374275c2c6e8bee52dfec1cb
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2022-12-18 18:49:14 +0700

    feat dbkb:wip

--- views/pages/pendataan/dbkb/nonStandar/jpb-2/index.php
+++ views/pages/pendataan/dbkb/nonStandar/jpb-2/index.php
@@ -4,12 +4,11 @@
 $scope = ' DBKB JPB 2';
 $action = 'Daftar';
 $showAdd = true;
-$addUrl = '/pendataan/dbkb/non-standar/jpb-2/tambah';
-$showOK = true;
+$addAsModal = true;
 
 $file = __DIR__.'/_components/list.php';
 $file_default = Yii::getAlias('@vwCompPath').'/list/defaultcontent.php';
 
 include Yii::getAlias('@vwCompPath/list/header.php');
 include file_exists($file) ? $file : $file_default;
-include Yii::getAlias('@vwCompPath/detail/footer.php');
+include Yii::getAlias('@vwCompPath/list/footer.php');
