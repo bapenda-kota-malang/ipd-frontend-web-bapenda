531d84322b2bec1df68c59b1976383a7a9fe7d6e
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2022-12-18 22:30:43 +0700

    feat dbkb: wip

--- views/pages/pendataan/dbkb/nonStandar/jpb-2/index.php
+++ views/pages/pendataan/dbkb/nonStandar/jpb-2/index.php
@@ -7,11 +7,9 @@ $showAdd = true;
 $addUrl = '/pendataan/dbkb/non-standar/jpb-2/tambah';
 $showOK = true;
 
-$file = __DIR__.'/_components/entryform.php';
+$file = __DIR__.'/_components/list.php';
 $file_default = Yii::getAlias('@vwCompPath').'/list/defaultcontent.php';
 
-// include Yii::getAlias('@vwCompPath/list/header.php');
-include Yii::getAlias('@vwCompPath/detail/header.php');
+include Yii::getAlias('@vwCompPath/list/header.php');
 include file_exists($file) ? $file : $file_default;
-// include Yii::getAlias('@vwCompPath/detail/footer.php');
 include Yii::getAlias('@vwCompPath/detail/footer.php');
