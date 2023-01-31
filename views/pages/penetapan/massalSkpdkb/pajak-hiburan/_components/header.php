8493fd637f3d4d4304ad26804730a250ef6c4817
bangadam <bangadam.dev@gmail.com>
2023-01-31 22:17:31 +0700

    slicing design - massal skpdkb pajak hiburan

---
+++ views/pages/penetapan/massalSkpdkb/pajak-hiburan/_components/header.php
@@ -0,0 +1,34 @@
+<div class="d-flex">
+    <div class="me-auto">
+        <h5 class="my-2">
+            <i class="bi bi-person"></i> <strong><?= isset($action) ? $action : '' ?></strong>
+            <?= isset($scope) ? $scope : '' ?>
+        </h5>
+    </div>
+    <div>
+        <div class="d-flex">
+            <div class="ms-auto">
+                <input v-model="searchKeywords" @change="search" class="form-control" placeholder="Cari..." style="width:400px" />
+                <!-- v-model="urls.dataSrcParams.searchKeywords" -->
+            </div>
+            <?php if (isset($showFilter)) { ?>
+                <div class="ms-2">
+                    <button @click="showFilter" class="btn bg-info">
+                        <i class="bi bi-sliders"></i> Filter
+                    </button>
+                </div>
+            <?php } ?>
+            <?php if (isset($showAdd)) { ?>
+                <div class="ms-3">
+                    <?php if (!isset($addAsModal) || !$addAsModal) { ?>
+                        <a href="<?= $addUrl ?>" class="btn bg-blue rounded-pill ">
+                            Salin dan Tetapkan
+                        </a>
+                    <?php } ?>
+                </div>
+            <?php } ?>
+            <?php if (isset($titleNav)) echo $titleNav ?>
+        </div>
+    </div>
+</div>
+<hr />
