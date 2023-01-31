11af3aac15a2cfc2439d71239c0d5b554542352c
bangadam <bangadam.dev@gmail.com>
2023-01-31 22:19:58 +0700

    slicing design - massal skpdkb pajak hotel

--- views/pages/penetapan/massalSkpdkb/pajak-hotel/index.php
+++ views/pages/penetapan/massalSkpdkb/pajak-hotel/index.php
@@ -5,11 +5,13 @@ $this->params['container_unset'] = true;
 $scope = ' Pajak Hotel';
 $action = 'Daftar';
 $showAdd = true;
+$showFilter = true;
 $addUrl = '/penetapan/massal-skpdkb/pajak-hotel/tambah';
+$currentUrl = '/penetapan/massal-skpdkb/pajak-hotel';
 
-$file = __DIR__.'/_components/list.php';
-$file_default = Yii::getAlias('@vwCompPath').'/list/defaultcontent.php';
+$file = __DIR__ . '/_components/list.php';
+$file_default = Yii::getAlias('@vwCompPath') . '/list/defaultcontent.php';
 
-include Yii::getAlias('@vwCompPath/list/header.php');
+include __DIR__ . '/_components/header.php';
 include file_exists($file) ? $file : $file_default;
 include Yii::getAlias('@vwCompPath/list/footer.php');
