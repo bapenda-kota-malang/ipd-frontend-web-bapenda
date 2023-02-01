7be3d5c8c18a1d02b30c911edeb61036e89f4c1e
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2023-02-01 09:09:39 +0700

    feat dbkb-fasum: wip

--- web/js/components/dbkb-fasum-item/dbkb-fasum-item.js
+++ web/js/components/dbkb-fasum-item/dbkb-fasum-item.js
@@ -3,12 +3,13 @@ Vue.component('dbkb-fasum-item', {
 		<table class="w-100" :class="!noMb ? 'mb-2' : ''">
 			<colgroup>
 				<col style="width:5%" />
-				<col style="width:30%" />
+				<col style="" />
 				<col style="width:5%" />
 				<col style="width:10%" />
 				<col style="width:10%" />
-				<col style="width:20%" />
-				<col style="width:20%" />
+				<col style="width:120px" />
+				<col style="width:120px" />
+				<col style="width:50px" />
 			</colgroup>
 			<thead v-if="!hideHeader">
 				<tr>
@@ -17,32 +18,128 @@ Vue.component('dbkb-fasum-item', {
 					<td>Satuan</td>
 					<td>Status</td>
 					<td>Ketergantungan</td>
-					<td>Nilai Lama</td>
-					<td>Nilai Baru</td>
+					<td class="text-end">Nilai Lama</td>
+					<td class="text-end">Nilai Baru</td>
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<td><input class="form-control" /></td>
-					<td><input class="form-control" /></td>
-					<td><input class="form-control" /></td>
-					<td><input class="form-control" /></td>
-					<td><input class="form-control" /></td>
-					<td><input class="form-control" /></td>
-					<td><input class="form-control" /></td>
+				<tr v-if="value && !Array.isArray(value)">
+					<td><input :value="data.kode" class="form-control" disabled /></td>
+					<td><input :value="data.nama" class="form-control" disabled /></td>
+					<td><input :value="data.satuan" class="form-control" disabled /></td>
+					<td><input :value="data.status" class="form-control" disabled /></td>
+					<td><input class="form-control" disabled /></td>
+					<td><input :value="value.nilaiLama" class="form-control text-end" disabled /></td>
+					<td><input v-model="nilaiBaru" class="form-control text-end" /></td>
+					<td class="nav">
+						<button v-if="showSaveButton" @click="save()" class="btn bg-blue"><i class="bi bi-check-lg"></i></button>
+					</td>
 				</tr>
-			</tbody>
+				<tr v-else-if="Array.isArray(value)" v-for="(item,idx) in value">
+					<td><input :value="data.kode" class="form-control" disabled /></td>
+					<td><input :value="data.nama + ' ' + item.nama" class="form-control" disabled /></td>
+					<td><input :value="data.satuan" class="form-control" disabled /></td>
+					<td><input :value="data.status" class="form-control" disabled /></td>
+					<td><input class="form-control" disabled /></td>
+					<td><input :value="item.nilaiLama" class="form-control text-end" disabled /></td>
+					<td><input v-model="nilaiBarus[idx]" class="form-control text-end" /></td>
+					<td class="nav">
+						<button v-if="showSaveButtons[idx]" @click="save(idx)" class="btn bg-blue"><i class="bi bi-check-lg"></i></button>
+					</td>
+				</tr>
+				<tr v-else>
+					<td>{{value}}</td>
+				</tr>
+				</tbody>
 		</table>`,
 	props: {
 		hideHeader: Boolean,
 		noMb: Boolean,
-	},
-	created: function(){
-		// console.log(hideHeader);
+		data: Object,
+		value: null,
+		url: '',
 	},
 	data:function() {
 		return {
-			data:{},
-		}	
+			nilaiBaru: 0,
+			nilaiBarus: [],
+			showSaveButton: false,
+			showSaveButtons: [],
+		}
+	},
+	created: function(){
+		// console.log(this.value);
+		// this.nilaiBaruAwal = this.value.nilaiBaru;
+	},
+	watch: {
+		value: function() {
+			if(this.value && !Array.isArray(this.value)) {
+				this.nilaiBaru = this.value.nilaiBaru;
+			}
+			if(this.value && Array.isArray(this.value)) {
+				xthis = this;
+				this.value.forEach(function(item, idx) {
+					xthis.nilaiBarus[idx] = item.nilaiBaru;
+					xthis.showSaveButtons[idx] = false;
+				})
+			}
+		},
+		nilaiBaru: function() {
+			if(this.nilaiBaru != this.value.nilaiBaru) {
+				this.showSaveButton = true;
+			} else {
+				this.showSaveButton = false;
+			}
+		},
+		nilaiBarus: function() {
+			xthis = this;
+			this.nilaiBarus.forEach(function(item, idx){
+				if(item != xthis.value[idx].nilaiBaru) {
+					xthis.showSaveButtons[idx] = true
+				} else {
+					xthis.showSaveButtons[idx] = false;
+				}
+			});
+		}
+	},
+	methods: {
+		async save(idx) {
+			value_id = null;
+			data = {
+				tahun: this.data.tahun,
+				provinsi_kode: this.data.provinsi_kode,
+				daerah_kode: this.data.daerah_kode,
+			}
+
+			if(!idx) {
+				if(!this.value.id) {
+					data.nilai = this.nilaiBaru;
+					res = await apiFetch(this.url, 'POST', data);
+				} else {
+					res = await apiFetch(`${this.ur}/${this.value.id}`, 'PATCH', {
+						nilai: this.nilaiBaru
+					});
+				}
+			} else {
+				value = values[idx];
+				if(!value.id) {
+					data = {
+						tahun: this.data.tahun,
+						nilai: this.nilaiBaru,
+					}
+					if(values[idx].klsBintang) {
+						data.klsBintang = value.klsBintang;
+					} else {
+						data.klsDepMin = value.klsDepMin;
+						data.klsDepMax = value.klsDepMax;
+					}
+					res = await apiFetch(this.url, 'POST', data);
+				} else {
+					res = await apiFetch(`${this.ur}/${value.id}`, 'PATCH', {
+						nilai: this.nilaiBarus[idx],
+					})
+				}
+			}
+		}
 	}
 });
--- web/js/services/dbkb/fasum-list.js
+++ web/js/services/dbkb/fasum-list.js
@@ -10,38 +10,180 @@ urls = {
 }
 
 vars = {
+	dataNonDepList: {},
+	dataBintangList: {},
+	dataMinMaxList:{},
+	dataNonDepLamaList: {},
+	dataBintangLamaList: {},
+	dataMinMaxLamaList:{},
+	dataSetList: {},
+	dataSetNilai: {},
 	searchKeywords:null,
 	tahun: today.getFullYear(),
-	dbkbFasumList: {},
-	dbkbFasumNonDepList: {},
-	dbkbFasumBintangList: {},
-	dbkbFasumMinMaxList:{},
+	provinsi_kode: '35',
+	daerah_kode: '73',
+	currentData: {},
+	lastyearData: {},
+	nonDepUrl: '/dbkbfasum/nondep',
+	minMaxUrl: '/dbkbfasum/depminmax',
+	bintangUrl: '/dbkbfasum/depjpbklsbintang',
 }
 refSources = {
-	dbkbFasumNonDepList: '/dbkbfasum',
-	dbkbFasumBintangList: '/dbkbfasum',
-	dbkbFasumMinMaxList: '/dbkbfasum',
 }
 watch = {
 }
 methods = {
+	setRefSources,
+	getData,
 }
 components = {
 	// dbkbFasumItem
+}
+
+function created() {
+	this.setRefSources();
 }
 
 function submitData() {
 
 }
 
+function setRefSources() {
+	this.refSources = {
+		dataNonDepList: `${this.nonDepUrl}?no_pagination=true&tahun=${this.tahun}`,
+		dataMinMaxList: `${this.minMaxUrl}?no_pagination=true&tahun=${this.tahun}`,
+		dataBintangList: `${this.bintangUrl}?no_pagination=true&tahun=${this.tahun}`,
+		dataNonDepLamaList: `${this.nonDepUrl}?no_pagination=true&tahun=${this.tahun-1}`,
+		dataMinMaxLamaList: `${this.minMaxUrl}?no_pagination=true&tahun=${this.tahun-1}`,
+		dataBintangLamaList: `${this.bintangUrl}?no_pagination=true&tahun=${this.tahun-1}`,
+	}
+}
+
+async function getData() {
+	this.setRefSources();
+	await this.checkRefSources();
+}
+
 function postFetchData(data) {
-	dbkbFasumList = this.dbkbFasumList;
+	xthis = this;
+	dataSetList = this.dataSetList; // this is diffrent inside forEach
+	dataSetNilai = this.dataSetNilai // this is diffrent inside forEach
 	data.forEach(function(item, idx){
-		dbkbFasumList[item.kode] = {
+		dataSetList[item.kode] = {
+			tahun: xthis.tahun,
+			provinsi_kode: xthis.provinsi_kode,
+			daerah_kode: xthis.daerah_kode,
+			kode: item.kode,
 			nama: item.nama,
-			items: [],
+			satuan: item.satuan,
+			status: item.status,
+			ketergantungan: item.ketergantungan,
+		}
+		// dataSetNilai[item.kode] = [];
+	});
+	this.$forceUpdate();
+}
+
+function postCheckRefSources() {
+	dataSetNilai = this.dataSetNilai // this is diffrent inside forEach
+	this.data.forEach(function(item, idx){
+		dataSetNilai[item.kode] = null;
+	});
+
+	// semua nilai baru
+	this.dataNonDepList.forEach(function(item) {
+		this.dataSetNilai[item.fasilitas_kode] = {
+			id: item.id,
+			nilaiLama: 0,
+			nilaiBaru: item.nilai,
 		}
 	});
-	console.log(this.dbkbFasumList);
+	this.dataBintangList.forEach(function(item) {
+		// worst case if inside loop
+		if(!this.dataSetNilai[item.fasilitas_kode]) {
+			this.dataSetNilai[item.fasilitas_kode] = [];	
+		}
+		this.dataSetNilai[item.fasilitas_kode].push({
+			id: item.id,
+			nama: ' KELAS BINTANG ' + item.klsBintang,
+			klsBintang: item.klsBintang,
+			nilaiLama: 0,
+			nilaiBaru: item.nilai,
+		});
+	});
+	this.dataMinMaxList.forEach(function(item) {
+		// worst case if inside loop
+		if(!this.dataSetNilai[item.fasilitas_kode]) {
+			this.dataSetNilai[item.fasilitas_kode] = [];	
+		}
+		this.dataSetNilai[item.fasilitas_kode].push({
+			id: item.id,
+			nama: ' DEP. MIN. ' + item.klsDepMin,
+			klsDepMin: item.klsDepMin,
+			klsDepMax: item.klsDepMax,
+			nilaiLama: 0,
+			nilaiBaru: item.nilai,
+		});
+	});
+
+	// nilai lama non dep
+	this.dataNonDepLamaList.forEach(function(item) {
+		if(this.dataSetNilai[item.fasilitas_kode]) {
+			this.dataSetNilai[item.fasilitas_kode].nilaiLama = item.nilai;
+		} else {
+			this.dataSetNilai[item.fasilitas_kode] = {
+				id: null,
+				nilaiLama: item.nilai,
+				nilaiBaru: 0,
+			};
+		}
+	});
+
+	// dep repopulation
+	for(prop in dataSetNilai) {
+		if(!dataSetNilai[prop]) {
+			dataSetNilai[prop] = [];
+		}
+	}
+
+	// nilai lama dep
+	this.dataBintangLamaList.forEach(function(item) {
+		filter = this.dataSetNilai[item.fasilitas_kode].filter(function(thisItem){
+			return item.klsBintang == thisItem.klsBintang;
+		});
+		
+		if(filter.length > 0) {
+			idx = this.dataSetNilai[item.fasilitas_kode].indexOf(filter[0]);
+			this.dataSetNilai[item.fasilitas_kode][idx].nilaiLama = item.nilai;
+		} else {
+			this.dataSetNilai[item.fasilitas_kode].push({
+				id: null,
+				nama: ' KELAS BINTANG ' + item.klsBintang,
+				bintang: item.klsBintang,
+				nilaiLama: item.nilai,
+				nilaiBaru: 0,
+			});
+		}
+	});
+	this.dataMinMaxLamaList.forEach(function(item) {
+		filter = this.dataSetNilai[item.fasilitas_kode].filter(function(thisItem){
+			return (item.klsDepMin == thisItem.klsDepMin && item.klsDepMax == thisItem.klsDepMax);
+		});
+		
+		if(filter.length > 0) {
+			idx = this.dataSetNilai[item.fasilitas_kode].indexOf(filter[0]);
+			this.dataSetNilai[item.fasilitas_kode][idx].nilaiLama = item.nilai;
+		} else {
+			this.dataSetNilai[item.fasilitas_kode].push({
+				id: null,
+				nama: ' DEP MIN ' + item.klsDepMin,
+				klsDepMin: item.klsDepMin,
+				klsDepMax: item.klsDepMax,
+				nilaiLama: item.nilai,
+				nilaiBaru: 0,
+			});
+		}
+	});
+
 	this.$forceUpdate();
 }
