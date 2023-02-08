54682b85097484f46467ea172b50a385e559a6b8
Hizkia Luke <contact.hizkia@gmail.com>
2023-02-08 22:03:09 +0700

    layouting 5.1

--- views/pages/penetapan/verifikasiESptpd/_components/detail.php
+++ views/pages/penetapan/verifikasiESptpd/_components/detail.php
@@ -27,7 +27,7 @@ $this->registerJsFile('@web/js/services/verifikasi-esptpd/detail.js?v=20221117a'
 		</div>
 		<div class="row g-1">
 			<div class="xc-md-4 xc-lg-3 xc-xl-2 pt-2">
-				Jenis Usaha
+				Jenis Pajak
 			</div>
 			<div class="xc-4 xc-lg-2 xc-xl-2 mb-2">
 				<input v-model="data.rekening.kode" class="form-control" disabled />
@@ -38,7 +38,7 @@ $this->registerJsFile('@web/js/services/verifikasi-esptpd/detail.js?v=20221117a'
 		</div>
 		<div class="row g-1">
 			<div class="xc-md-4 xc-lg-3 xc-xl-2 pt-2">
-				Nama Usaha
+				Nama
 			</div>
 			<div class="col-md-8 col-lg-6 col-xl-5 col-xxl-4 mb-2">
 				<input v-model="data.objekPajak.nama" class="form-control" disabled />
@@ -63,16 +63,16 @@ $this->registerJsFile('@web/js/services/verifikasi-esptpd/detail.js?v=20221117a'
 		</div>
 		<div class="row g-1">
 			<div class="xc-md-4 xc-lg-3 xc-xl-2 pt-1">
-				Kelurahan
-			</div>
-			<div class="xc-md-6 col-lg-7 col-xl-6">
-				<input v-model="data.objekPajak.kelurahan.nama" class="form-control" disabled />
-			</div>
-			<div class="xc-md-4 xc-lg-3 xc-xl-2 pt-1 pe-2 text-md-end">
 				Kecamatan
 			</div>
 			<div class="xc-md-6 col-lg-7 col-xl-6">
 				<input v-model="data.objekPajak.kecamatan.nama" class="form-control" disabled />
+			</div>
+			<div class="xc-md-4 xc-lg-3 xc-xl-2 pt-1 pe-2 text-md-end">
+				Kelurahan
+			</div>
+			<div class="xc-md-6 col-lg-7 col-xl-6">
+				<input v-model="data.objekPajak.kelurahan.nama" class="form-control" disabled />
 			</div>
 		</div>
 	</div>
@@ -292,7 +292,7 @@ $this->registerJsFile('@web/js/services/verifikasi-esptpd/detail.js?v=20221117a'
 				<div class="col-md-4">
 					<div class="row g-1">
 						<div class="col-md-4">
-							<input v-model="data.dataDetails[idx].jumlahJam" class="form-control" disabled>						
+							<input v-model="data.dataDetails[idx].jumlahJam" class="form-control" disabled>
 						</div>
 						<div class="col-md-4">
 							<input v-model="data.dataDetails[idx].jumlahHari" class="form-control" disabled>
@@ -346,49 +346,95 @@ $this->registerJsFile('@web/js/services/verifikasi-esptpd/detail.js?v=20221117a'
 <div class="card mb-3">
 	<div class="card-header">Estimasi Perhitungan</div>
 	<div class="p-3">
-		<div v-if="!rekening_objek" class="p-3 text-center">
+		<!-- <div v-if="!rekening_objek" class="p-3 text-center">
 			<i class="bi bi-info-circle"></i> Menunggu informasi jenis pajak...
 		</div>
-		<div v-else>
+		<div v-else> -->
 		<div class="row g-1">
-				<div class="xc-md-4 xc-lg-2 pt-1">Nomor SPT</div>
-				<div class="xc-md-6 xc-lg-4 mb-2"><input value="otomatis" class="form-control" disabled /></div>
-				<div class="xc-md-4 xc-lg-3 pt-1 text-md-end pe-md-2">Billing</div>
-				<div class="xc-md-6 xc-lg-4 mb-2"><input value="otomatis" class="form-control" disabled /></div>
-				<div class="xc-md-4 xc-lg-3 pt-1 text-lg-end pe-2">Virtual Account</div>
-				<div class="xc-md-6 xc-lg-4 xc-lg-3 mb-3"><input value="otomatis" class="form-control" disabled /></div>
-			</div>
-			<div class="row g-1">
-				<div class="xc-md-4 xc-lg-2 pt-1">Periode Awal</div>
-				<div class="xc-md-6 xc-lg-4 mb-2"><input v-model="data.periodeAwal" class="form-control" disabled /></div>
-				<div class="xc-md-4 xc-lg-3 pt-1 text-md-end pe-2">Periode Awal</div>
-				<div class="xc-md-6 xc-lg-4 mb-2"><input v-model="data.periodeAkhir" class="form-control" disabled /></div>
-				<div class="xc-md-4 xc-lg-3 pt-1 text-lg-end pe-2">Jatuh Tempo</div>
-				<div class="xc-md-6 xc-lg-4 xc-lg-3 mb-3"><input v-model="data.jatuhTempo" class="form-control" disabled /></div>
-			</div>
-			<div class="row g-1">
-				<div class="xc-md-4 xc-lg-2 pt-1">Subtotal E-Tax</div>
-				<div class="xc-md-6 xc-lg-4 mb-2"><input class="form-control" disabled /></div>
-				<div class="xc-md-4 xc-lg-3 pt-1 text-md-end pe-2">E-Tax</div>
-				<div class="xc-md-6 xc-lg-4 mb-2"><input class="form-control" disabled /></div>
-				<div class="xc-md-4 xc-lg-3 pt-1 text-lg-end pe-2">Total E-Tax</div>
-				<div class="xc-md-6 xc-lg-4 xc-lg-3 mb-3"><input class="form-control" disabled /></div>
-			</div>
-			<div class="row g-1">
-				<div class="xc-md-4 xc-lg-2 pt-1">Potensi</div>
-				<div class="xc-md-6 xc-lg-4 mb-2"><input v-model="data.omset" class="form-control" disabled /></div>
-				<div class="xc-md-4 xc-lg-3 pt-1 text-md-end pe-2">Tarif Pajak (%)</div>
-				<div class="xc-md-6 xc-lg-4 mb-2"><input v-model="data.tarifPajak.tarifPersen" class="form-control" disabled /></div>
-				<div class="xc-md-4 xc-lg-3 pt-1 text-lg-end pe-2">Jumlah</div>
-				<div class="xc-md-6 xc-lg-4 xc-lg-3 mb-3"><input v-model="data.jumlahPajak" class="form-control" disabled /></div>
-			</div>
-			<div class="row g-1 mt-2">
-				<div class="xc-md-4 xc-lg-2 pt-1">Keterangan</div>
-				<div class="xc-md xc-lg-14 xc-lg-12 xc-lg-10 mb-2">
-					<textarea class="form-control" disabled></textarea>
+			<div class="xc-md-4 xc-lg-2 pt-1">Nomor SPT</div>
+			<div class="xc-md-6 xc-lg-4 mb-2"><input value="otomatis" class="form-control" disabled /></div>
+			<div class="xc-md-4 xc-lg-3 pt-1 text-md-end pe-md-2">Billing</div>
+			<div class="xc-md-6 xc-lg-4 mb-2"><input value="otomatis" class="form-control" disabled /></div>
+			<div class="xc-md-4 xc-lg-3 pt-1 text-lg-end pe-2">Virtual Account</div>
+			<div class="xc-md-6 xc-lg-4 xc-lg-3 mb-3"><input value="otomatis" class="form-control" disabled /></div>
+		</div>
+		<div class="row g-1">
+			<div class="xc-md-4 xc-lg-2 pt-1">Periode Awal</div>
+			<div class="xc-md-6 xc-lg-4 mb-2"><input v-model="data.periodeAwal" class="form-control" disabled /></div>
+			<div class="xc-md-4 xc-lg-3 pt-1 text-md-end pe-2">Periode Awal</div>
+			<div class="xc-md-6 xc-lg-4 mb-2"><input v-model="data.periodeAkhir" class="form-control" disabled /></div>
+			<div class="xc-md-4 xc-lg-3 pt-1 text-lg-end pe-2">Jatuh Tempo</div>
+			<div class="xc-md-6 xc-lg-4 xc-lg-3 mb-3"><input v-model="data.jatuhTempo" class="form-control" disabled /></div>
+		</div>
+		<div class="row g-1">
+			<div class="xc-md-4 xc-lg-2 pt-1">Subtotal E-Tax</div>
+			<div class="xc-md-6 xc-lg-4 mb-2"><input class="form-control" disabled /></div>
+			<div class="xc-md-4 xc-lg-3 pt-1 text-md-end pe-2">E-Tax</div>
+			<div class="xc-md-6 xc-lg-4 mb-2"><input class="form-control" disabled /></div>
+			<div class="xc-md-4 xc-lg-3 pt-1 text-lg-end pe-2">Total E-Tax</div>
+			<div class="xc-md-6 xc-lg-4 xc-lg-3 mb-3"><input class="form-control" disabled /></div>
+		</div>
+		<div class="row g-1">
+			<div class="xc-md-4 xc-lg-2 pt-1">Potensi</div>
+			<div class="xc-md-6 xc-lg-4 mb-2"><input v-model="data.omset" class="form-control" disabled /></div>
+			<div class="xc-md-4 xc-lg-3 pt-1 text-md-end pe-2">Tarif Pajak (%)</div>
+			<div class="xc-md-6 xc-lg-4 mb-2"><input v-model="data.tarifPajak.tarifPersen" class="form-control" disabled /></div>
+			<div class="xc-md-4 xc-lg-3 pt-1 text-lg-end pe-2">Jumlah</div>
+			<div class="xc-md-6 xc-lg-4 xc-lg-3 mb-3"><input v-model="data.jumlahPajak" class="form-control" disabled /></div>
+		</div>
+		<div class="row g-1 mt-2">
+			<div class="col-md-3 pt-1">Menggunakan Kas Register / komputer / pos :</div>
+			<div class="col-md-6 mb-2">
+				<div class="row pt-1">
+					<div class="col-md-3">
+						<div class="form-check">
+							<input v-model="data.kas" class="form-check-input" type="radio" name="kasRadio" id="kasRadio1" value="ya" />
+							<label class="form-check-label" for="kasRadio1">
+								Ya
+							</label>
+						</div>
+					</div>
+					<div class="col mb-1">
+						<div class="form-check">
+							<input v-model="data.kas" class="form-check-input" type="radio" name="kasRadio" id="kasRadio2" value="tidak" />
+							<label class="form-check-label" for="kasRadio2">
+								Tidak
+							</label>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
+		<div class="row g-1 mt-2">
+			<div class="col-md-3 pt-1">Mengadakan Pembukuan / Pencatatan :</div>
+			<div class="col-md-6 mb-2">
+				<div class="row pt-1">
+					<div class="col-md-3">
+						<div class="form-check">
+							<input v-model="data.pembukuan" class="form-check-input" type="radio" name="pembukuanRadio" id="pembukuanRadio1" value="ya" />
+							<label class="form-check-label" for="pembukuanRadio1">
+								Ya
+							</label>
+						</div>
+					</div>
+					<div class="col mb-1">
+						<div class="form-check">
+							<input v-model="data.pembukuan" class="form-check-input" type="radio" name="pembukuanRadio" id="pembukuanRadio2" value="tidak" />
+							<label class="form-check-label" for="pembukuanRadio2">
+								Tidak
+							</label>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row g-1 mt-2">
+			<div class="xc-md-4 xc-lg-2 pt-1">Keterangan</div>
+			<div class="xc-md xc-lg-14 xc-lg-12 xc-lg-10 mb-2">
+				<textarea class="form-control" disabled></textarea>
+			</div>
+		</div>
+		<!-- </div> -->
 	</div>
 </div>
 <div class="card mb-3">
@@ -403,4 +449,3 @@ $this->registerJsFile('@web/js/services/verifikasi-esptpd/detail.js?v=20221117a'
 		</div>
 	</div>
 </div>
-
--- views/pages/penetapan/verifikasiESptpd/pajak-penerangan-jalan/_components/entryform.php
+++ views/pages/penetapan/verifikasiESptpd/pajak-penerangan-jalan/_components/entryform.php
@@ -1,28 +1,70 @@
-<?php include __DIR__.'/../../_components/entryform_reg.php' ?>
+<?php include __DIR__ . '/../../_components/entryform_reg.php' ?>
+
+<?php
+$data = [
+	['Sosial', '&nbsp;', '&nbsp;', '0%', '&nbsp;'],
+	['Rumah Tangga', '&nbsp;', '&nbsp;', '7%', '&nbsp;'],
+	['Bisnis', '&nbsp;', '&nbsp;', '5%', '&nbsp;'],
+	['Industri', '&nbsp;', '&nbsp;', '3%', '&nbsp;'],
+	['Pemerintah', '&nbsp;', '&nbsp;', '0%', '&nbsp;'],
+]
+?>
 
 <div class="card mb-4">
 	<div class="card-header">Data Detail Objek Pajak</div>
 	<div class="card-body">
-		<table class="table table-bordered">
+		<?php foreach ($data as $item) { ?>
+			<strong class=""> <?= $item[0] ?> </strong>
+			<table class="table table-bordered mt-1">
+				<thead>
+					<tr>
+						<th style="width:25%">Jumlah Pelanggan</th>
+						<th style="width:25%">Jumlah Rekening</th>
+						<th style="width:25%">Tarif</th>
+						<th style="width:25%">Jumlah</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td> <?= $item[1] ?> </td>
+						<td> <?= $item[2] ?> </td>
+						<td> <?= $item[3] ?> </td>
+						<td> <?= $item[4] ?> </td>
+					</tr>
+				</tbody>
+			</table>
+		<?php } ?>
+
+		<strong class=""> Lain-lain </strong>
+		<table class="table table-bordered mt-1">
 			<thead>
 				<tr>
-					<th class="text-center" style="width:50px">
-						<input class="form-check-input" type="checkbox">
-					</th>
-					<th>Golongan Kamar</th>
-					<th>Tarif</th>
-					<th style="width:20%">Jumlah Kamar</th>
-					<th style="width:20%">Jumlah Kamar Yang Laku</th>
+					<th style="width:25%">Jumlah Pelanggan</th>
+					<th style="width:25%">Jumlah Rekening</th>
+					<th style="width:50%">Jumlah</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
-					<td colspan="6" class="text-center">Belum ada data</td>
+					<td> &nbsp; </td>
+					<td> &nbsp; </td>
+					<td> &nbsp; </td>
 				</tr>
 			</tbody>
 		</table>
+
+		<div>
+			<div class="row g-1">
+				<div class="xc-md-4 xc-lg-2 pt-1">&nbsp;</div>
+				<div class="xc-md-6 xc-lg-4 mb-2">&nbsp;</div>
+				<div class="xc-md-4 xc-lg-3 pt-1 text-md-end pe-md-2">&nbsp;</div>
+				<div class="xc-md-6 xc-lg-4 mb-2">&nbsp;</div>
+				<div class="xc-md-4 xc-lg-3 pt-1 text-lg-end pe-2"><strong>Jumlah Pajak</strong></div>
+				<div class="xc-md-6 xc-lg-4 xc-lg-3 mb-3"><input value="Rp. " class="form-control" /></div>
+			</div>
+		</div>
 	</div>
 </div>
 
-<?php include __DIR__.'/../../_components/entryform_est.php' ?>
-<?php include __DIR__.'/../../_components/entryform_lamp.php' ?>
+<?php include __DIR__ . '/../../_components/entryform_rwyt.php' ?>
+<?php include __DIR__ . '/../../_components/entryform_lamp.php' ?>
