84610a564db1abb2e1884ad830e56d685614f1cc
Hizkia Luke <contact.hizkia@gmail.com>
2023-02-10 09:48:02 +0700

    layouting menu 9, 13, 14

--- views/pages/bendahara/sinkronisasi-data-pembayaran-bank/index.php
+++ views/pages/bendahara/sinkronisasi-data-pembayaran-bank/index.php
@@ -7,8 +7,8 @@ $action = 'Daftar';
 $showAdd = true;
 $addUrl = '/bendahara/sinkronisasi-data-pembayaran-bank/tambah';
 
-$file = __DIR__.'/_components/list.php';
-$file_default = Yii::getAlias('@vwCompPath').'/list/defaultcontent.php';
+$file = __DIR__ . '/_components/list.php';
+$file_default = Yii::getAlias('@vwCompPath') . '/list/defaultcontent.php';
 
 include Yii::getAlias('@vwCompPath/list/header.php');
 include file_exists($file) ? $file : $file_default;
