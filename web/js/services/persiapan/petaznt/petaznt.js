95f20c11a27a3969ac341a4cd82d0a2899ed19fe
K421Prabowo <kusuma.ari.prabowo@gmail.com>
2023-02-14 14:09:25 +0700

    update peta znt not saving

--- web/js/services/persiapan/petaznt/petaznt.js
+++ web/js/services/persiapan/petaznt/petaznt.js
@@ -164,7 +164,7 @@ async function hapusZnt(idx) {
 function preSubmit(xthis) {
 	data = xthis.data;
 
-    lenDatas = data.datas.length - 1;
+    lenDatas = data.datas.legitngth - 1;
 	if (data.datas[lenDatas].znt_kode == null || data.datas[lenDatas].znt_kode == "") {
         data.datas.splice(lenDatas, 1);
     }
