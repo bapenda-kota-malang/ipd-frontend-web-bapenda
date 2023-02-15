5821b18e020a1784499139488ac56ad9352bae9a
bangadam <bangadam.dev@gmail.com>
2023-02-15 21:16:44 +0700

    16.2.3.1 Harga Resource √ 16.3.7 No Pelayanan √ 16.3.8 NOP terbesar √

---
+++ views/pages/lihat/nomor-pelayanan/_components/header.php
@@ -0,0 +1,9 @@
+<div class="d-flex">
+    <div class="me-auto">
+        <h5 class="my-2">
+            <i class="bi bi-person"></i> <strong><?= isset($action) ? $action : '' ?></strong>
+            <?= isset($scope) ? $scope : '' ?>
+        </h5>
+    </div>
+</div>
+<hr />
--- views/pages/pengurangan/cetak-sk/index.php
+++ views/pages/pengurangan/cetak-sk/index.php
@@ -3,13 +3,13 @@
 $this->params['container_unset'] = true;
 
 $scope = ' Cetak SK Pengurangan';
-$action = 'Daftar';
+$action = '';
 $showAdd = true;
 $addUrl = '/pengurangan/cetak-sk/tambah';
 
-$file = __DIR__.'/_components/list.php';
-$file_default = Yii::getAlias('@vwCompPath').'/list/defaultcontent.php';
+$file = __DIR__ . '/_components/entryform.php';
+$file_default = Yii::getAlias('@vwCompPath') . '/list/defaultcontent.php';
 
-include Yii::getAlias('@vwCompPath/list/header.php');
+include __DIR__ . '/_components/header.php';
 include file_exists($file) ? $file : $file_default;
 include Yii::getAlias('@vwCompPath/list/footer.php');
