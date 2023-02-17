361826d22ac2e80e27ad5e39e0b9505e0dff3276
bangadam <bangadam.dev@gmail.com>
2023-02-17 21:18:16 +0700

    layouting konfigurasi pajak dan rekening

---
+++ views/pages/konfigurasi/pajak/pdl/rekening-pajak/_components/header.php
@@ -0,0 +1,46 @@
+<div class="d-flex">
+    <div class="me-auto">
+        <h5 class="my-2">
+            <i class="bi bi-person"></i> <strong><?= isset($action) ? $action : '' ?></strong>
+            <?= isset($scope) ? $scope : '' ?>
+        </h5>
+    </div>
+    <div>
+        <div class="d-flex">
+            <div class="ms-auto">
+                <input v-model="searchKeywords" @change="search" class="form-control" placeholder="Cari..." style="width:400px" />
+                <!-- v-model="urls.dataSrcParams.searchKeywords" -->
+            </div>
+            <?php if (isset($showFilter)) { ?>
+                <div class="ms-2">
+                    <button @click="showFilter" class="btn bg-info">
+                        <i class="bi bi-sliders"></i> Filter
+                    </button>
+                </div>
+            <?php } ?>
+            <?php if (isset($showAdd)) { ?>
+                <div class="ms-3">
+                    <div class="btn-group">
+                        <button data-bs-toggle="dropdown" class="btn bg-blue">
+                            <i class="bi bi-plus"></i> Tambah
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modal-pajak">
+                                    Pajak
+                                </button>
+                            </li>
+                            <li>
+                                <button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modal-rekening">
+                                    Rekening
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            <?php } ?>
+            <?php if (isset($titleNav)) echo $titleNav ?>
+        </div>
+    </div>
+</div>
+<hr />
