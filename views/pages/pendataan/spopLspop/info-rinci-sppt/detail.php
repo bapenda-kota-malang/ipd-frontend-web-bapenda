6b20af9ea6e67935e8e0c769bcfb7a291d0f3cbd
riefive <rie.five@gmail.com>
2023-03-07 06:33:19 +0700

    chore: layouting informasi sppt

--- views/pages/pendataan/spopLspop/info-rinci-sppt/detail.php
+++ views/pages/pendataan/spopLspop/info-rinci-sppt/detail.php
@@ -4,10 +4,10 @@ $scope = ' Informasi Rinci SPPT';
 $action = 'Detail';
 $showBack = true;
 $backUrl = '/pendataan/spop-lspop/info-rinci-sppt';
-$showEdit = true;
+$showEdit = null;
 
 $editUrl = '/pendataan/spop-lspop/info-rinci-sppt/'.$id.'/edit';
-$file = __DIR__.'/_components/detail.php';
+$file = __DIR__.'/_components/entryform.php';
 $file_default = Yii::getAlias('@vwCompPath').'/detail/defaultcontent.php';
 
 include Yii::getAlias('@vwCompPath/detail/header.php');
