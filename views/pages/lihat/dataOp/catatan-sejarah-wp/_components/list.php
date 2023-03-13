9a079625e1cae2302e6f068f9afcb54179194e5e
bangadam <bangadam.dev@gmail.com>
2023-03-13 21:02:25 +0700

    delete 2 files and update 2 files

--- views/pages/lihat/dataOp/catatan-sejarah-wp/_components/list.php
+++ views/pages/lihat/dataOp/catatan-sejarah-wp/_components/list.php
@@ -5,37 +5,34 @@ use app\assets\VueAppListLegacyAsset;
 
 VueAppListLegacyAsset::register($this);
 
-// $this->registerCssFile('https://unpkg.com/vue2-datepicker/index.css', ["position" => View::POS_HEAD]);
-// $this->registerJsFile('https://unpkg.com/vue2-datepicker/index.min.js', ["position" => View::POS_HEAD]);
-
-// $this->registerCssFile('https://unpkg.com/vue-select@3.20.0/dist/vue-select.css', ["position" => View::POS_HEAD]);
-// $this->registerJsFile('https://unpkg.com/vue-select@3.20.0', ["position" => View::POS_HEAD]);
-
-$this->registerJsFile('@web/js/services/jaminan-bongkar/list.js?v=20221108a');
+$this->registerJsFile('@web/js/helper/nop.js?v=20221108a');
+$this->registerJsFile('@web/js/services/catatan-sejarah-wp/list.js?v=20221108a');
 
 ?>
 
 <div>
+	<div class="row">
+		<div class="col-1 text-left">NOP</div>
+		<div class="col-11 d-flex">
+			<?php
+			$nopName = 'nopFields';
+			include Yii::getAlias('@vwCompPath/bscope/nop-input.php');
+			?>
+			<button class="btn btn-block btn-primary" @click="onClickBtnCari">Cari</button>
+		</div>
+	</div>
 	<div class="row g-2">
 		<div class="col-6">
-			<div class="row">
-				<div class="col-3 text-left">NOP</div>
-				<div class="col-9">
-					<?php
-					include Yii::getAlias('@vwCompPath/bscope/nop-input.php');
-					?>
-				</div>
-			</div>
 			<div class="row mt-2">
 				<div class="col-3 text-left">Alamat Wajib Pajak</div>
 				<div class="col">
-					<input type="text" name="" id="" class="form-control">
+					<input type="text" v-model="data.alamatObjekPajak" class="form-control" disabled>
 				</div>
 			</div>
 			<div class="row mt-2">
 				<div class="col-3 text-left">Kelurahan</div>
 				<div class="col">
-					<select name="" class="form-control" id=""></select>
+					<input type="text" v-model="data.kelurahan" class="form-control" disabled>
 				</div>
 			</div>
 		</div>
@@ -43,22 +40,15 @@ $this->registerJsFile('@web/js/services/jaminan-bongkar/list.js?v=20221108a');
 			<div class="row mt-2">
 				<div class="col-3 text-left">RT / RW</div>
 				<div class="col">
-					<input type="text" name="" id="" class="form-control">
+					<input type="text" v-model="data.rt_rw" class="form-control" disabled>
 				</div>
 			</div>
 			<div class="row mt-2">
 				<div class="col-3 text-left">Luas Tanah</div>
 				<div class="col">
-					<input type="text" name="" id="" class="form-control">
+					<input type="text" v-model="data.luasTanah" class="form-control" disabled>
 				</div>
 			</div>
-		</div>
-	</div>
-
-
-	<div class="row">
-		<div class="col-4">
-			<button class="btn btn-block btn-primary">Cari</button>
 		</div>
 	</div>
 </div>
@@ -67,36 +57,21 @@ $this->registerJsFile('@web/js/services/jaminan-bongkar/list.js?v=20221108a');
 <table class="table custom">
 	<thead>
 		<tr>
-			<th style="width:50px"><input class="form-check-input" type="checkbox" value=""></th>
 			<th>Tahun Pajak</th>
 			<th>Tanggal Cetak</th>
 			<th>Tanggal Terbit</th>
 			<th>Tanggal Jatuh Tempo</th>
 			<th>Nama Wajib Pajak</th>
 			<th>Alamat Wajib Pajak</th>
-			<th style="width:90px"></th>
 		</tr>
 	<tbody>
-		<tr v-for="item in 5">
-			<td><input type="checkbox" /></td>
-			<td>lorem</td>
-			<td>lorem</td>
-			<td>lorem</td>
-			<td>lorem</td>
-			<td>lorem</td>
-			<td>lorem</td>
-			<td>
-				<div class="btn-group">
-					<button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-						Aksi
-					</button>
-					<ul class="dropdown-menu">
-						<li><a class="dropdown-item" href="#">Detail</a></li>
-						<li><a class="dropdown-item" href="#">Edit</a></li>
-						<li><a class="dropdown-item" href="#">Hapus</a></li>
-					</ul>
-				</div>
-			</td>
+		<tr v-for="(item, idx) in data.list" :key="idx">
+			<td>{{item.tahunPajak}}</td>
+			<td>{{item.tanggalCetak}}</td>
+			<td>{{item.tanggalJatuhTempo}}</td>
+			<td>{{item.tanggalTerbit}}</td>
+			<td>{{item.namaWajibPajak}}</td>
+			<td>{{item.alamatWajibPajak}}</td>
 		</tr>
 	</tbody>
 	</thead>
--- views/pages/lihat/dataOp/catatan-sejarah-wp/index.php
+++ views/pages/lihat/dataOp/catatan-sejarah-wp/index.php
@@ -4,11 +4,9 @@ $this->params['container_unset'] = true;
 
 $scope = ' Catatan Sejarah WP';
 $action = 'Daftar';
-$showAdd = true;
-$addUrl = '/konfigurasi/lihat/data-op/catatan-sejarah-wp/tambah';
 
-$file = __DIR__.'/_components/list.php';
-$file_default = Yii::getAlias('@vwCompPath').'/list/defaultcontent.php';
+$file = __DIR__ . '/_components/list.php';
+$file_default = Yii::getAlias('@vwCompPath') . '/list/defaultcontent.php';
 
 include Yii::getAlias('@vwCompPath/list/header.php');
 include file_exists($file) ? $file : $file_default;
