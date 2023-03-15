9739232363bb2e34f89bc97c5a229aeeb132a1d6
riefive <rie.five@gmail.com>
2023-03-15 13:06:23 +0700

    chore: relayout sejarah sppt

--- views/pages/lihat/sejarah/sppt/index.php
+++ views/pages/lihat/sejarah/sppt/index.php
@@ -1,15 +1,18 @@
 <?php
-
-$this->params['container_unset'] = true;
+$this->params['content_fixed'] = true;
+$this->params['container_unset'] = null;
 
 $scope = ' Sejarah SPPT';
 $action = 'Daftar';
-$showAdd = true;
+$showSearch = false;
+$showAdd = null;
 $addUrl = '/konfigurasi/lihat/sejarah/sppt/tambah';
+$withYear = true;
+$screenType = 'center';
 
 $file = __DIR__.'/_components/list.php';
 $file_default = Yii::getAlias('@vwCompPath').'/list/defaultcontent.php';
 
-include Yii::getAlias('@vwCompPath/list/header.php');
+include $screenType == 'full' ? Yii::getAlias('@vwCompPath/list/header.php') : Yii::getAlias('@vwCompPath/detail/header.php');
 include file_exists($file) ? $file : $file_default;
-include Yii::getAlias('@vwCompPath/list/footer.php');
+include $screenType == 'full' ? Yii::getAlias('@vwCompPath/list/footer.php') : Yii::getAlias('@vwCompPath/detail/footer.php');
