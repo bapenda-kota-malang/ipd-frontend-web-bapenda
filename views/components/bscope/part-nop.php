6b2e242e7cb03d7c3acfc0bbb7ccd9c6c3705b14
riefive <rie.five@gmail.com>
2023-03-16 14:31:02 +0700

    chore: modify table nomor pelayanan

--- views/components/bscope/part-nop.php
+++ views/components/bscope/part-nop.php
@@ -1,9 +1,8 @@
 <div class="row align-items-center mb-2">
-    <div class="col-2 text-left text-uppercase">NOP</div>
-		<div class="col-4">
+    <div class="col-1 text-left text-uppercase">NOP</div>
+		<div class="col-3">
 		  <?php include Yii::getAlias('@vwCompPath/bscope/nop-input.php'); ?>
 		</div>
-    <div class="col-2"></div>
     <div class="col-2">
       <button type="button" class="btn btn-outline-secondary w-100">Cari</button>
     </div>
--- views/pages/lihat/nomor-pelayanan/_components/list.php
+++ views/pages/lihat/nomor-pelayanan/_components/list.php
@@ -9,25 +9,21 @@ $this->registerJsFile('@web/js/services/lihat/sejarah-sppt/list.js?v=20221108a')
 ?>
 
 <?php include Yii::getAlias('@vwCompPath/bscope/part-nop.php'); ?>
-<div class="card mb-4">
-</div>
-
-<div>
-	<div class="table-responsive">
-		<table class="table custom">
-			<thead>
+<div class="mt-4">
+	<div class="table-responsive">	
+		<table class="table table-bordered custom">
+			<thead class="thead">
 				<tr>
-					<th>Nomor Pelayanan</th>
-					<th>Tahun</th>
-					<th>Jenis Pelayanan</th>
+					<th scope="col" class="text-center text-uppercase">Nomor Pelayanan</th>
+					<th scope="col" class="text-center text-uppercase">Tahun</th>
+					<th scope="col" class="text-center text-uppercase">Jenis Pelayanan</th>
 				</tr>
 			<tbody>
-				<tr v-for="(item, index) in 5">
-					<td>2023-0001-015</td>
-					<td>2022</td>
-					<td>09 - Restitusi Dan Kompensasi</td>
+				<tr v-for="i in 10" :key="i">
+					<td v-for="j in 3" :key="j">&nbsp;</td>
 				</tr>
 			</tbody>
 			</thead>
 		</table>
 	</div>
+</div>
