e4e0642fca5df09d22e06ce16f66d48197df19cc
riefive <rie.five@gmail.com>
2023-03-20 16:17:40 +0700

    chore: layouting detail verifikasi pdl tahap 3

---
+++ views/pages/pengurangan/verifikasi-pdl/_components/part-form-04.php
@@ -0,0 +1,13 @@
+<div class="p-3">
+  <div class="row align-items-center g-3 mb-3">
+    <div class="col-6 d-flex justify-content-start align-items-center">
+      <div class="text-start w-25 me-1">Petugas Verifikasi</div>
+      <input type="text" class="form-control w-50" disabled>
+    </div>
+    <div class="col-1"></div>
+    <div class="col-4 d-flex justify-content-start align-items-center">
+      <div class="text-start me-3">Tanggal Verifikasi</div>
+      <input type="text" class="form-control w-50" disabled>
+    </div>
+  </div>
+</div>
