d139970e652096e1984b29daefee2f15a2694da9
riefive <rie.five@gmail.com>
2023-03-21 16:27:08 +0700

    chore: create part attachment

--- views/pages/pengurangan/pajak-daerah/_components/entryform.php
+++ views/pages/pengurangan/pajak-daerah/_components/entryform.php
@@ -7,6 +7,7 @@ VueAppEntryFormLegacyAsset::register($this);
 $this->registerCssFile('https://unpkg.com/vue2-datepicker/index.css', ["position" => View::POS_HEAD]);
 $this->registerJsFile('https://unpkg.com/vue2-datepicker/index.min.js', ["position" => View::POS_HEAD]);
 
+$attachType = 'notfull';
 $paramJobName = Yii::$app->getRequest()->getQueryParam('job_name');
 $this->registerJsFile('@web/js/services/pengurangan/entry-pajak-daerah.js?v=20221108a');
 ?>
@@ -54,6 +55,8 @@ $this->registerJsFile('@web/js/services/pengurangan/entry-pajak-daerah.js?v=2022
 	</div>
   <hr />
 </div>
+
+<?php include Yii::getAlias('@vwCompPath/bscope/part-pajak-daerah-attachment.php'); ?>
 
 <div class="row mb-2">
 	<div class="col d-flex justify-content-between align-items-center mb-2">
