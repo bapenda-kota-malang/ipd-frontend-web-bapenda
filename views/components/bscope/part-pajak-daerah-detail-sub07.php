ab9551f6187e31564b9bd0d68859711771d8803a
riefive <rie.five@gmail.com>
2023-03-23 14:02:02 +0700

    chore: modify background thead part pajak detail

--- views/components/bscope/part-pajak-daerah-detail-sub07.php
+++ views/components/bscope/part-pajak-daerah-detail-sub07.php
@@ -2,7 +2,7 @@
   <div class="col-12" style="font-weight: 600">Detail Objek Pajak Penerangan Jalan PLN</div>
   <div class="col-12">
     <table class="table table-bordered custom">
-      <thead class="thead">
+      <thead class="thead" style="background: #B9B9B9">
         <tr>
           <th scope="col" class="text-center" style="width: 5%">No</th>
           <th scope="col" class="text-center">Jenis PPJ</th>
@@ -11,12 +11,12 @@
           <th scope="col" class="text-center">Tarif</th>
           <th scope="col" class="text-center">Jumlah</th>
         </tr>
+      </thead>
       <tbody>
         <tr v-for="i in 1" :key="i">
           <td v-for="j in 6" :key="j">&nbsp;</td>
         </tr>
       </tbody>
-      </thead>
     </table>
   </div>
 </div>
