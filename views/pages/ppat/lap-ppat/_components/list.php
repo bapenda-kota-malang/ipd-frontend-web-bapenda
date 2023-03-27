290c8fae17bf920c0f2ebdfa39adaf4a198ce40e
riefive <rie.five@gmail.com>
2023-03-27 11:31:57 +0700

    chore: add list laporan ppat

--- views/pages/ppat/lap-ppat/_components/list.php
+++ views/pages/ppat/lap-ppat/_components/list.php
@@ -15,3 +15,38 @@ $this->registerJsFile('@web/js/services/ppat/list-laporan.js?v=20221108a');
 ?>
 
 <?php include Yii::getAlias('@vwCompPath/bscope/part-filter-ppat.php'); ?>
+
+<hr>
+
+<div class="table-responsive-xl p-2">
+  <table class="table table-bordered custom">
+    <thead class="thead">
+      <tr style="background-color: #B9B9B9">
+        <th scope="col" class="text-center" style="width: 5%">No</th>
+        <th scope="col" class="text-center">Nama PPAT</th>
+        <th scope="col" class="text-center">Tanggal Laporan</th>
+        <th scope="col" class="text-center">Jumlah Transaksi</th>
+        <th scope="col" class="text-center">NOMINAL TRANSAKSI (Rp.)</th>
+        <th scope="col" class="text-center">NOMINAL BPHTB (Rp.)</th>
+        <th scope="col" class="text-center">Status</th>
+        <th scope="col" class="text-center">Aksi</th>
+      </tr>
+    <tbody>
+      <tr v-for="(item, i) in data.lists" :key="i">
+        <td>{{ i+1 }}</td>
+        <td>{{ item.nama }}</td>
+        <td>{{ item.tanggalText }}</td>
+        <td>{{ item.jumlahTransaksiText }}</td>
+        <td>{{ item.nominalTransaksiText }}</td>
+        <td>{{ item.nominalBphtbText }}</td>
+        <td class="text-center">{{ item.statusText }}</td>
+        <td class="text-center">
+          <a :href="`/ppat/lap-ppat/detail/${item.id}`">
+           <i class="bi bi-eye-fill" style="font-size: 24px"></i>
+          </a>
+        </td>
+      </tr>
+    </tbody>
+    </thead>
+  </table>
+</div>
