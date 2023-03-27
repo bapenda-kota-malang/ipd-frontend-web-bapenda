0201b42f983d7ba5d39178513e68381c59f2b0d4
riefive <rie.five@gmail.com>
2023-03-27 13:31:44 +0700

    chore: update detail laporan ppat

--- views/pages/ppat/lap-ppat/_components/detail.php
+++ views/pages/ppat/lap-ppat/_components/detail.php
@@ -21,23 +21,31 @@ $this->registerJsFile('@web/js/services/ppat/detail-laporan.js?v=20221108a');
   <table class="table table-bordered table-horizontal-dynamic" cellspacing="0">
     <thead class="thead">
       <tr style="background-color: #B9B9B9">
+        <th scope="col" colspan="2" class="text-center">Akta</th>
+        <th scope="col" colspan="6" class="text-center">Luas</th>
+        <th scope="col" colspan="2" class="text-center">Nama, Alamat dan NPWP</th>
+        <th scope="col" colspan="2" class="text-center">SSP</th>
+        <th scope="col" colspan="2" class="text-center"></th>
+        <th scope="col" colspan="3" class="text-center">SSPD</th>
+      </tr>
+      <tr style="background-color: #B9B9B9">
         <th scope="col" class="text-center" style="width: 5%">No</th>
         <th scope="col" class="text-center">Tanggal Akta</th>
         <th scope="col" class="text-center">Letak Tanah</th>
         <th scope="col" class="text-center">Tanah</th>
         <th scope="col" class="text-center">Bangunan</th>
-        <th scope="col" class="text-center">Luas NOP</th>
+        <th scope="col" class="text-center">NOP</th>
         <th scope="col" class="text-center">NJOP (Rp.)</th>
         <th scope="col" class="text-center">Harga Transaksi</th>
         <th scope="col" class="text-center">Pihak Yang Mengalihkan</th>
         <th scope="col" class="text-center">Pihak Yang Menerima</th>
-        <th scope="col" class="text-center">Tgl. SSP</th>
-        <th scope="col" class="text-center">Nominal SSP (Rp.)</th>
+        <th scope="col" class="text-center">Tgl</th>
+        <th scope="col" class="text-center">Nominal (Rp.)</th>
         <th scope="col" class="text-center">Bentuk Pembuatan atau Hukum</th>
         <th scope="col" class="text-center">Jenis dan Hak</th>
-        <th scope="col" class="text-center">No. SSPD</th>
-        <th scope="col" class="text-center">Tgl. SSPD</th>
-        <th scope="col" class="text-center">Nominal SSPD (Rp.)</th>
+        <th scope="col" class="text-center">No</th>
+        <th scope="col" class="text-center">Tgl</th>
+        <th scope="col" class="text-center">Nominal (Rp.)</th>
       </tr>
     <tbody>
       <tr v-for="(item, i) in data.lists" :key="i">
@@ -46,7 +54,7 @@ $this->registerJsFile('@web/js/services/ppat/detail-laporan.js?v=20221108a');
         <td>{{ item.letakTanah }}</td>
         <td>{{ item.tanah }}</td>
         <td>{{ item.bangunan }}</td>
-        <td>{{ item.luasNop }}</td>
+        <td>{{ item.nop }}</td>
         <td>{{ item.nominalNjop }}</td>
         <td>{{ item.harga }}</td>
         <td>{{ item.pihak1 }}</td>
--- web/js/services/ppat/detail-laporan.js
+++ web/js/services/ppat/detail-laporan.js
@@ -4,7 +4,7 @@ data = {
 	dummies: [
 		{ 
       id: 123, tanggalAkta: '12/02/2022', letakTanah: 'Jl Gajayana 13 Kelurahan Sumbersari Kecamatan Lowokwaru Kota Malang', tanah: 100, bangunan: 80, 
-      luasNop: '35.73.040.010.005.0087.0', nominalNjop: '400000000', harga: '400000000', pihak1: 'Cipto Mangunbroto', pihak2: 'Supeno',
+      nop: '35.73.040.010.005.0087.0', nominalNjop: '400000000', harga: '400000000', pihak1: 'Cipto Mangunbroto', pihak2: 'Supeno',
       tanggalSsp: '10/30/2022', nominalSsp: '20000', bentukHukum: 'Jual Beli', jenisHak: 'SHM 34 1998', noSSPD: '29.10.2022.4000.000', 
       tanggalSspd: '10/29/2022', nominalSspd: '20000'
     }
