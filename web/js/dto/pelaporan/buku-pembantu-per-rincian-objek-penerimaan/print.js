cabe872448d5ae23dd937f9901e1936db4d25a49
Ittofiq Surgawan <ittofiq@gmail.com>
2023-04-12 13:55:01 +0700

    Pelaporan Buku Pembantu Per Rincian Objek Penerimaan

---
+++ web/js/dto/pelaporan/buku-pembantu-per-rincian-objek-penerimaan/print.js
@@ -0,0 +1,5 @@
+printEntry = {
+    jenisPajak: null,
+    tanggalAwal: new Date(),
+    tanggalAkhir: new Date(),
+}
---
+++ web/js/services/pelaporan/buku-pembantu-per-rincian-objek-penerimaan/print.js
@@ -0,0 +1,30 @@
+data = {...printEntry}
+
+vars = {
+    pajakList: [
+        {pajak: 'Penerimaan Pajak Hotel'},
+		{pajak: 'Penerimaan Pajak Restoran'},
+        {pajak: 'Penerimaan Pajak Hiburan'},
+        {pajak: 'Penerimaan Pajak Parkir'},
+        {pajak: 'Penerimaan Pajak Reklame'},
+        {pajak: 'Penerimaan Pajak Air Bawah Tanah'},
+        {pajak: 'Penerimaan Pajak Penerangan Jalan'},
+    ],
+}
+
+refSources = {
+    submitCetak: '',
+}
+
+methods = {
+    submitCetak,
+}
+
+components = {
+    datepicker: DatePicker,
+	vueselect: VueSelect.VueSelect,
+}
+
+function submitCetak() {
+    
+}
